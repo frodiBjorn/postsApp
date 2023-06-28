adf34e96b17641440848ad54426a3941aaf6c9e5
Ivan Fedotovsky <fedotovsky96@gmail.com>
2023-06-28 20:37:03 +0300

    fix3 error url

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -15,7 +15,7 @@ const AppRouter = () => {
     isAuth
       ?
       <Routes>
-        <Route path="postsApp/login" element={<Navigate to="postsApp/posts"/>} />
+        <Route path="/login" element={<Navigate to="/posts"/>} />
         {privateRoutes.map(route => {
           return (
             <Route
@@ -29,7 +29,7 @@ const AppRouter = () => {
       </Routes>
       :
       <Routes>
-        <Route path="*" element={<Navigate to="postsApp/login" replace/>} />
+        <Route path="/*" element={<Navigate to="/login" replace/>} />
         {publicRoutes.map(route => {
           return (
             <Route
